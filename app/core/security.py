73547f358a28af713928ed2f05eaf9a2f2fe9a7e
ABDCODE <pathana2@montclair.edu>
2025-04-11 11:54:50 -0400

    Create security.py

---
+++ app/core/security.py
@@ -0,0 +1,12 @@
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from datetime import datetime, timedelta
+from app.core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm="HS256")
