fa166e194ca5453c4bd6fd7d1f437ee497be8362
ABDCODE <pathana2@montclair.edu>
2025-04-11 11:59:36 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,11 @@
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic import BaseSettings
 from typing import List
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     ALPHA_VANTAGE_API_KEY: str
-    FINNHUB_API_KEY: str
-    BACKEND_CORS_ORIGINS: List[str] = [
-        "http://localhost:3000",
-        "https://actinovate-frontend.vercel.app"
-    ]
+    BACKEND_CORS_ORIGINS: List[str] = ["*"]
     SECRET_KEY: str = "your-secret-key-here"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
     class Config:
         env_file = ".env"
